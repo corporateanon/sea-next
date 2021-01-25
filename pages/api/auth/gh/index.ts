055049b124c5a16548f18e888afecab4d120ad06
corporateanon@gmail.com <corporateanon>
2021-01-25 20:48:37 +0200

    it works somehow

--- pages/api/auth/gh/index.ts
+++ pages/api/auth/gh/index.ts
@@ -6,6 +6,7 @@ const clientID = process.env.GH_CLIENT_ID;
 const endpoint = async (req: NextApiRequest, res: NextApiResponse) => {
     const params = stringify({
         client_id: clientID,
+        scope: 'read:user'
     });
 
     res.redirect(`https://github.com/login/oauth/authorize?${params}`);
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,43 @@
-import Head from 'next/head';
+import { gql, useQuery } from '@apollo/client';
 import { FC } from 'react';
 
+const getRepos = gql`
+    query getRepos($searchQuery: String!) {
+        search(query: $searchQuery, type: REPOSITORY, first: 10) {
+            edges {
+                node {
+                    ... on Repository {
+                        id
+                        name
+                        stargazerCount
+                        forkCount
+                        nameWithOwner
+                    }
+                }
+            }
+        }
+    }
+`;
+
 const Home: FC = () => {
-    return <div>aaaa</div>;
+    const { data, loading, error } = useQuery(getRepos, {
+        variables: {
+            searchQuery: 're',
+        },
+    });
+    return (
+        <>
+            <ul>
+                {data?.search?.edges &&
+                    data?.search?.edges.map(({ node }, i) => (
+                        <li key={i}>
+                            {node.nameWithOwner} 🌟 {node.stargazerCount} 🍴{' '}
+                            {node.forkCount}
+                        </li>
+                    ))}
+            </ul>
+        </>
+    );
 };
 
 export default Home;
